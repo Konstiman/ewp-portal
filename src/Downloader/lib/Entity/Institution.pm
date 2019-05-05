a8010f5eead288f8a7320ffc68bbdece37597028
Jan Konstant <jankonstant@gmail.com>
2019-05-05 17:20:54 +0200

    class attributes modified at module + diagram

--- src/Downloader/lib/Entity/Institution.pm
+++ src/Downloader/lib/Entity/Institution.pm
@@ -7,6 +7,84 @@ has 'id' => (
     isa => 'Int'
 );
 
+has 'identifier' => (
+	is => 'rw',
+    isa => 'Str'
+);
+
+has 'otherIdentifiers' => (
+	is => 'rw',
+    isa => 'HashRef[Str]',
+    traits => ['Hash'],
+    default => sub { { } },
+    handles => {
+        setOtherIdentifier => set
+    }
+);
+
+has 'names' => (
+	is => 'rw',
+    isa => 'HashRef[Str]',
+    traits => ['Hash'],
+    default => sub { { } },
+    handles => {
+        setName => set
+    }
+);
+
+has 'abbreviation' => (
+	is => 'rw',
+    isa => 'Str'
+);
+
+has 'logoUrl' => (
+	is => 'rw',
+    isa => 'Str'
+);
+
+has 'websites' => (
+	is => 'rw',
+    isa => 'HashRef[Str]',
+    traits => ['Hash'],
+    default => sub { { } },
+    handles => {
+        setWebsite => set
+    }
+);
+
+has 'factsheets' => (
+	is => 'rw',
+    isa => 'HashRef[Str]',
+    traits => ['Hash'],
+    default => sub { { } },
+    handles => {
+        setFactsheet => set
+    }
+);
+
+has 'mailingAddress' => (
+	is => 'rw',
+    # TODO objekt
+    isa => 'Str'
+);
+
+has 'locationAddress' => (
+	is => 'rw',
+    # TODO objekt
+    isa => 'Str'
+);
+
+has 'contacts' => (
+	is => 'rw',
+    # TODO pole objektu
+    isa => 'ArrayRef[Str]',
+    traits => ['Array'],
+    default => sub { [ ] },
+    handles => {
+        addContact => push
+    }
+);
+
 no Moose;
 __PACKAGE__->meta->make_immutable;
 
