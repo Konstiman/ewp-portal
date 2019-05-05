cffa3cefe788f812061e8966d5255bb6cf624023
Jan Konstant <jankonstant@gmail.com>
2019-05-05 11:29:05 +0200

    directory structure changed

---
+++ src/Downloader/lib/Institution.pm
@@ -0,0 +1,21 @@
+package Institution;
+ 
+use strict;
+use warnings;
+ 
+sub new {
+	my $class  = shift;
+	my %params = @_;
+	
+    my $self = bless { 
+		id => ( $params{ id } || undef ),
+		indentifier => $params{ identifier },
+		# TODO nazvy a identifikatory
+    }, $class;
+	
+	return $self;
+}
+
+# TODO gettery a settery
+ 
+1;
