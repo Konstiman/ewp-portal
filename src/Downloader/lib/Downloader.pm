64447f054c5f2ea7e05f4d3173d55ae8592f7f1a
Jan Konstant <jankonstant@gmail.com>
2019-05-05 11:56:39 +0200

    using Moose; moving functions to module Downloader

--- src/Downloader/lib/Downloader.pm
+++ src/Downloader/lib/Downloader.pm
@@ -1,21 +1,48 @@
 package Downloader;
- 
-use strict;
-use warnings;
- 
-sub new {
-	my $class  = shift;
-	my %params = @_;
-	
-    my $self = bless { 
-		id => ( $params{ id } || undef ),
-		indentifier => $params{ identifier },
-		# TODO nazvy a identifikatory
-    }, $class;
-	
-	return $self;
+
+use Moose;
+
+use HTTP::Request;
+use LWP::UserAgent;
+
+use constant CATALOGUE_URL =>
+  'https://registry.erasmuswithoutpaper.eu/catalogue-v1.xml';
+
+use constant VERSION => '1.0.0';
+
+=pod
+
+=head1 DESCRIPTION
+
+Modul slouzi ke stazeni a ulozeni XML katalogu instituci Erasmus Without Paper.
+
+=head1 METHODS
+
+=head2 C<downloadCatalogue () : Str>
+
+Stahne XML katalog z adresy C<catalogueUrl>.
+
+=cut
+
+sub downloadCatalogue {
+    my $self = shift;
+
+    my $agent = LWP::UserAgent->new(
+        agent      => 'EWP_Catalogue_Downloader/' . VERSION,
+        cookie_jar => {}
+    );
+    my $request = HTTP::Request->new( GET => CATALOGUE_URL );
+
+    my $response = $agent->request( $request );
+
+    if ( !$response->is_success ) {
+        die 'Catalogue download failed: ' . $response->status_line;
+    }
+
+    my $content = $response->content;
+
+    return $content;
 }
 
-# TODO gettery a settery
- 
-1;
+no Moose;
+__PACKAGE__->meta->make_immutable;
